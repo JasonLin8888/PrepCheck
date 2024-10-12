61678d80cfa70ed93488859abf16d154b5a77a6a
Nickolas-Ellison1 <nwellison@college.harvard.edu>
2024-10-12 21:36:50 +0000

    Add NurseChatBot.py

--- app/NurseChatBot.py
+++ app/NurseChatBot.py
@@ -3,7 +3,7 @@ import os
 # Import the OpenAI library to interact with OpenAI's API
 from openai import OpenAI
 
-client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+#client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 import gradio
 
 
@@ -13,6 +13,7 @@ def CustomChatGPT(user_input):
     messages.append({"role": "user", "content": user_input})
     response = client.chat.completions.create(model = "gpt-3.5-turbo",
     messages = messages)
+
     ChatGPT_reply = response.choices[0].message.content
     messages.append({"role": "assistant", "content": ChatGPT_reply})
     return ChatGPT_reply
